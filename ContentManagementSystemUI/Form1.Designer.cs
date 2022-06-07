29bb93060f5ecac2c2fc681c38e31c2fdfc35af6
Tristan Jordan <tristan.jordan@live.com>
2022-06-06 17:03:58 -0700

    Initial commit

---
+++ ContentManagementSystemUI/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ContentManagementSystemUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titleTxtBox = new System.Windows.Forms.TextBox();
+            this.blogTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.blogPostBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(777, 363);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(424, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Date";
+            // 
+            // titleTxtBox
+            // 
+            this.titleTxtBox.Location = new System.Drawing.Point(77, 42);
+            this.titleTxtBox.Name = "titleTxtBox";
+            this.titleTxtBox.Size = new System.Drawing.Size(296, 20);
+            this.titleTxtBox.TabIndex = 3;
+            // 
+            // blogTxtBox
+            // 
+            this.blogTxtBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blogTxtBox.Location = new System.Drawing.Point(36, 83);
+            this.blogTxtBox.Multiline = true;
+            this.blogTxtBox.Name = "blogTxtBox";
+            this.blogTxtBox.Size = new System.Drawing.Size(732, 264);
+            this.blogTxtBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(369, 350);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Background Color:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Font Color:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 402);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Blog Post";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // blogPostBox
+            // 
+            this.blogPostBox.BackColor = System.Drawing.SystemColors.Control;
+            this.blogPostBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.blogPostBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blogPostBox.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.blogPostBox.Location = new System.Drawing.Point(8, 425);
+            this.blogPostBox.Multiline = true;
+            this.blogPostBox.Name = "blogPostBox";
+            this.blogPostBox.Size = new System.Drawing.Size(776, 385);
+            this.blogPostBox.TabIndex = 11;
+            this.blogPostBox.TextChanged += new System.EventHandler(this.blogPostBox_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(473, 40);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(212, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Create Post";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Black",
+            "White",
+            "Red",
+            "Orange",
+            "LightBlue",
+            "Pink ",
+            "Green"});
+            this.comboBox1.Location = new System.Drawing.Point(117, 350);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(212, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "White",
+            "Black",
+            "Red",
+            "Orange",
+            "LightBlue",
+            "Pink ",
+            "Green"});
+            this.comboBox2.Location = new System.Drawing.Point(519, 350);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(212, 21);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 813);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.blogPostBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.blogTxtBox);
+            this.Controls.Add(this.titleTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Content Management System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox titleTxtBox;
+        private System.Windows.Forms.TextBox blogTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox blogPostBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ ContentManagementSystemUI/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContentManagementSystemUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        public  string MakeBlogPost()
+        {
+            string title, date, post;
+            title = titleTxtBox.Text.ToString();
+            date = dateTimePicker1.Value.ToString();
+            post = blogTxtBox.Text.ToString();
+            return $"Title: {title}\n\n" + $"{" "}" +
+                $"Date: {date}\n\n" + $"{" "}" + $"{post}";
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Method to put it in the Blog TextBox
+            blogPostBox.Text= MakeBlogPost();
+           
+            //Forground Color
+            if (comboBox1.SelectedIndex == 0)
+            {
+                blogTxtBox.ForeColor = Color.Black;
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                blogPostBox.ForeColor = Color.White;
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                blogPostBox.ForeColor = Color.Red;
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                blogPostBox.ForeColor = Color.Orange;
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                blogPostBox.ForeColor = Color.LightBlue;
+            }
+            else if (comboBox1.SelectedIndex == 5)
+            {
+                blogPostBox.ForeColor = Color.Pink;
+            }
+            else if (comboBox1.SelectedIndex == 6)
+            {
+                blogPostBox.ForeColor = Color.Green;
+            }
+
+            //Background Color
+            if (comboBox2.SelectedIndex == 0)
+            {
+                blogPostBox.BackColor = Color.White;
+            }
+            else if (comboBox2.SelectedIndex == 1)
+            {
+                blogPostBox.BackColor = Color.Black;
+            }
+            else if (comboBox2.SelectedIndex == 2)
+            {
+                blogPostBox.BackColor = Color.Red;
+            }
+            else if (comboBox2.SelectedIndex == 3)
+            {
+                blogPostBox.BackColor = Color.Orange;
+            }
+            else if (comboBox2.SelectedIndex == 4)
+            {
+                blogPostBox.BackColor = Color.LightBlue;
+            }
+            else if (comboBox2.SelectedIndex == 5)
+            {
+                blogPostBox.BackColor = Color.Pink;
+            }
+            else if (comboBox2.SelectedIndex == 6)
+            {
+                blogPostBox.BackColor = Color.Green;
+            }
+
+
+          
+        }
+      
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           //if(comboBox1.SelectedIndex == 0)
+           // {
+           //     blogTxtBox.ForeColor = Color.Black;
+           // }
+           //else if(comboBox1.SelectedIndex == 1)
+           // {
+           //     blogPostBox.ForeColor = Color.White;
+           // }
+           // else if (comboBox1.SelectedIndex == 1)
+           // {
+           //     blogPostBox.ForeColor = Color.Red;
+           // }
+           // else if (comboBox1.SelectedIndex == 1)
+           // {
+           //     blogPostBox.ForeColor = Color.Orange;
+           // }
+           // else if (comboBox1.SelectedIndex == 1)
+           // {
+           //     blogPostBox.ForeColor = Color.LightBlue;
+           // }
+           // else if (comboBox1.SelectedIndex == 1)
+           // {
+           //     blogPostBox.ForeColor = Color.Pink;
+           // }
+           // else if (comboBox1.SelectedIndex == 1)
+           // {
+           //     blogPostBox.ForeColor = Color.Green;
+           // }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //BackGround Color
+            //if (comboBox2.SelectedIndex == 0)
+            //{
+            //    blogPostBox.BackColor = Color.White;
+            //}
+            //else if (comboBox2.SelectedIndex == 1)
+            //{
+            //    blogPostBox.BackColor = Color.Black;
+            //}
+            //else if (comboBox2.SelectedIndex == 2)
+            //{
+            //    blogPostBox.BackColor = Color.Red;
+            //}
+            //else if (comboBox2.SelectedIndex == 3)
+            //{
+            //    blogPostBox.BackColor = Color.Orange;
+            //}
+            //else if (comboBox2.SelectedIndex == 4)
+            //{
+            //    blogPostBox.BackColor = Color.LightBlue;
+            //}
+            //else if (comboBox2.SelectedIndex == 5)
+            //{
+            //    blogPostBox.BackColor = Color.Pink;
+            //}
+            //else if (comboBox2.SelectedIndex == 6)
+            //{
+            //    blogPostBox.BackColor = Color.Green;
+            //}
+        }
+
+        private void blogPostBox_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
